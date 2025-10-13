2a39c130370e1e9e4a10491f3bb0ed6af21d87d6
RehanAnsari17 <165806706+RehanAnsari17@users.noreply.github.com>
2025-10-13 23:04:44 +0530

    Update firebase.ts

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // TODO: Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "fill",
-  authDomain: "fill",
-  projectId: "fill",
-  storageBucket: "fill",
-  messagingSenderId: "fill",
-  appId: "fill"
+  apiKey: "AIzaSyBBNJzQHpkcrUAbPeGFMkQBl-A9uEBruws",
+  authDomain: "restaurant-chatbot-84524.firebaseapp.com",
+  projectId: "restaurant-chatbot-84524",
+  storageBucket: "restaurant-chatbot-84524.firebasestorage.app",
+  messagingSenderId: "19931230266",
+  appId: "1:19931230266:web:a83b3272313c790e070503"
 };
 
 // Initialize Firebase
